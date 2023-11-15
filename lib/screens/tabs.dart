bfa69df65af63e90c4b4e46fa75d78346e19c435
maithanhtrong1113 <maithanhtrong1113@gmail.com>
2023-11-15 17:50:16 +0700

    add drawer

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frist_app/models/meal.dart';
-
 import 'package:frist_app/screens/categories.dart';
+import 'package:frist_app/screens/fillters.dart';
 import 'package:frist_app/screens/meals.dart';
 import 'package:frist_app/widgets/main_drawer.dart';
 
@@ -50,6 +50,17 @@ class _TabsScreenState extends State<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
+    void _setScreen(String identifier) {
+      Navigator.of(context).pop();
+      if (identifier == 'filters') {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (ctx) => const FiltersScreen(),
+          ),
+        );
+      }
+    }
+
     Widget activePage = CategoriesScreen(
       onToggleFavorite: _toggleMealFavoriteStatus,
     );
@@ -65,6 +76,19 @@ class _TabsScreenState extends State<TabsScreen> {
 
     return Scaffold(
       appBar: AppBar(
+        leading: Builder(builder: (context) {
+          return IconButton(
+            icon: Icon(
+              Icons.format_list_bulleted_outlined,
+              size: 30,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+          );
+        }),
         title: Text(
           activePageTitle,
           style: Theme.of(context).textTheme.titleLarge!.copyWith(
@@ -74,7 +98,7 @@ class _TabsScreenState extends State<TabsScreen> {
         ),
       ),
       body: activePage,
-      drawer: const MainDrawer(),
+      drawer: MainDrawer(onSelectScreen: _setScreen),
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         currentIndex: _selectedPageIndex,
